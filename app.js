d01a24b7a6019f83cdf4765f5aa22f60af564613
Safolya <safolyamondal12345@gmail.com>
2025-06-27 02:00:40 +0530

    read create

--- app.js
+++ app.js
@@ -5,16 +5,44 @@ const path=require("path");
 const app = express();
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
     res.send("hello");
 });
-
+//index route
 app.get("/listing",async(req,res)=>{
     const alllistings=await listingModel.find({});
     res.render("listings/index.ejs",{alllistings});
+});
+
+//New List Route
+app.get("/listing/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+app.post("/listing/create",async(req,res)=>{
+    let {title,description,image,country,location,price}=req.body;
+    let newlist=await listingModel.create({
+        title:title ,
+        description: description,
+        country: country,
+        location: location ,
+        price: price
+    });
+    newlist.save();
+    console.log(newlist);
+    res.redirect("/listing");
 })
+
+//show route
+app.get("/listing/:id",async(req,res)=>{
+    let {id}=req.params;
+    let list=await listingModel.findById(id);
+    res.render("listings/show.ejs",{list});
+});
+
+
 /*app.get("/testlist", async (req, res) => {
     let newList =await  listingModel.create({
         title: "My House",
