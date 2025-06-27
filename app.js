b8f0fb087dbce7e59d967db3bf32245882fe3630
Safolya <safolyamondal12345@gmail.com>
2025-06-27 14:02:57 +0530

    update create

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const db = require("./config/mongoose-connection");
 const listingModel=require("./models/listing");
 const path=require("path");
 const app = express();
+const methodOverride=require("method-override");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("hello");
@@ -21,6 +23,7 @@ app.get("/listing/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
 
+//create route
 app.post("/listing/create",async(req,res)=>{
     let {title,description,image,country,location,price}=req.body;
     let newlist=await listingModel.create({
@@ -33,7 +36,21 @@ app.post("/listing/create",async(req,res)=>{
     newlist.save();
     console.log(newlist);
     res.redirect("/listing");
-})
+});
+
+//edit route
+app.get("/listing/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let list=await listingModel.findById(id);
+    res.render("listings/edit.ejs",{list});
+});
+
+//update route
+app.put("/listing/:id",async(req,res)=>{
+    let {id}=req.params;
+    let list=await listingModel.findByIdAndUpdate(id,{...req.body.list});
+    res.redirect("/listing");
+});
 
 //show route
 app.get("/listing/:id",async(req,res)=>{
