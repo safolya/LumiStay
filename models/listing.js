ae0230cdc429642119813c229577918e9b52b12c
Safolya <safolyamondal12345@gmail.com>
2025-06-29 01:47:18 +0530

    image

--- models/listing.js
+++ models/listing.js
@@ -14,7 +14,6 @@ const listingSchema=new mongoose.Schema({
         default:"https://images.unsplash.com/photo-1748367959778-12d026a20a99?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v)=> v === ""? "https://images.unsplash.com/photo-1748367959778-12d026a20a99?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
       } ,
-      filename:String,
     },
     country:String,
     location:String,
